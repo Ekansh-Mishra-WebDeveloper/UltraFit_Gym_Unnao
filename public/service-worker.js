5753a9b57393826e010061bedcd9ec768dbbd6e8
Ekansh Mishra <ekanshmishra124@gmail.com>
2026-05-31 14:50:55 +0530

    download fix_fuckkkk

--- public/service-worker.js
+++ public/service-worker.js
@@ -7,20 +7,24 @@ const urlsToCache = [
   '/members.html',
   '/shop.html',
   '/index.css',
-  '/index.js',          // add your main JS file
+  '/index.js',
   '/ultrafit-logo.png'
 ];
 
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache).catch(err => {
+        console.error('Failed to cache one or more files:', err);
+      });
+    })
   );
 });
 
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
   );
 });
